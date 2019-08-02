d5e9d1f98aa003a242fa84634ea34dca38ad5ecf
magael <mikael.jaakkola@helsinki.fi>
2019-08-03 00:37:41 +0300

    testing Node

---
+++ aastaar/src/test/java/map/NodeTest.java
@@ -0,0 +1,54 @@
+package map;
+
+import mj.aastaar.map.Node;
+import org.junit.Before;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author MJ
+ */
+public class NodeTest {
+    
+    Node nodeA, nodeB, nodeC, nodeD;
+    
+    @Before
+    public void setUp() {
+        nodeA = new Node(0, 6);
+        nodeB = new Node(34, 170);
+        nodeC = new Node(0, 99);
+        nodeD = new Node(0, 6);
+    }
+    
+    @Test
+    public void equalsReturnsTrueForSameObject() {
+        assertTrue((nodeA.equals(nodeA)));
+    }
+    
+    @Test
+    public void equalsReturnsTrueForDifferentButEqual() {
+        assertTrue((nodeA.equals(nodeD)));
+    }
+    
+    @Test
+    public void equalsReturnsFalseForDifferent() {
+        assertFalse((nodeA.equals(nodeB)));
+    }
+    
+    @Test
+    public void equalsReturnsFalseForDifferentOnSameRow() {
+        assertFalse((nodeA.equals(nodeC)));
+    }
+    
+    @Test
+    public void equalsReturnsFalseForNull() {
+        assertFalse((nodeA.equals(null)));
+    }
+    
+    @Test
+    public void equalsReturnsFalseForDifferentClass() {
+        String example = "not a node";
+        assertFalse((nodeA.equals(example)));
+    }
+}
