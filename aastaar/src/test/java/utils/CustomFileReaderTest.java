3303a3f261bfb0542b33b7410580dae793de4d7b
magael <mikael.jaakkola@helsinki.fi>
2019-08-02 18:39:22 +0300

    some filereader tests and trying to get gradle to work from console

---
+++ aastaar/src/test/java/utils/CustomFileReaderTest.java
@@ -0,0 +1,48 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package utils;
+
+import java.util.ArrayList;
+import org.junit.Before;
+import org.junit.Test;
+import static org.junit.Assert.*;
+import mj.aastaar.utils.CustomFileReader;
+
+/**
+ *
+ * @author MJ
+ */
+public class CustomFileReaderTest {
+
+    private CustomFileReader reader;
+    private String filePath;
+
+    @Before
+    public void setUp() {
+        reader = new CustomFileReader();
+        filePath = "testmaps/test.map";
+    }
+
+    @Test
+    public void readFileReturnsCorrectData() throws Exception {
+        reader.readFile(filePath);
+        String[] dataArray = reader.getDataArray();
+        assertEquals("type octile", dataArray[0]);
+        assertEquals("width 4", dataArray[1]);
+        assertEquals("height 4", dataArray[2]);
+        assertEquals("T@@T", dataArray[3]);
+        assertEquals("T..T", dataArray[4]);
+        assertEquals("TW.T", dataArray[5]);
+        assertEquals("TTTT", dataArray[6]);
+        assertEquals(7, dataArray.length);
+    }
+
+    @Test(expected = Exception.class)
+    public void throwsExceptionIfBadFilePath() throws Exception {
+        String noSuchPath = "wrong/no_file.bad";
+        reader.readFile(noSuchPath);
+    }
+}
