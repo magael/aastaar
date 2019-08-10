3d19974519e9dd8a03b8670dd04f30800353f661
Mikael Jaakkola <mikael.jaakkola@helsinki.fi>
2019-08-10 20:39:35 +0300

    rm comments

--- aastaar/src/main/java/mj/aastaar/algorithms/path/PathWithArray.java
+++ aastaar/src/main/java/mj/aastaar/algorithms/path/PathWithArray.java
@@ -8,26 +8,18 @@ import mj.aastaar.map.Node;
  */
 public class PathWithArray implements Path {
 
-    //public HashMap<Node, Node> cameFrom;
     public Node[][] cameFrom;
 
     public PathWithArray(int nx, int ny) {
-        //cameFrom = new HashMap<>();
         cameFrom = new Node[nx][ny];
     }
 
-//    public HashMap<Node, Node> getCameFrom() {
-//        return cameFrom;
-//    }
     public Node[][] getCameFrom() {
         return cameFrom;
     }
 
     @Override
     public boolean containsNode(Node node) {
-//        if (cameFrom.containsKey(node)) {
-//            return true;
-//        }
         if (cameFrom[node.getX()][node.getY()] != null) {
             return true;
         }
@@ -36,7 +28,6 @@ public class PathWithArray implements Path {
 
     @Override
     public void putCameFrom(Node to, Node from) {
-//        cameFrom.put(to, from);
         cameFrom[to.getX()][to.getY()] = from;
     }
 
@@ -45,7 +36,6 @@ public class PathWithArray implements Path {
     public int earlyExit(Node current, Node start) {
         int steps = 0;
         while (!current.equals(start)) {
-//            current = cameFrom.get(current);
             current = cameFrom[current.getX()][current.getY()];
             steps++;
         }
@@ -55,7 +45,6 @@ public class PathWithArray implements Path {
     // returns the Nodes of the shortest path after the path and it's length are found
     // TODO: no separate shortestPlathLength and shortestPathLength methods,
     // always return the shortest path.
-    // would still require 2 iterations of the path or a dynamic or a very large array
     @Override
     public Node[] shortestPath(Node goal, Node start, int length) {
         if (cameFrom == null || length < 1) {
@@ -67,7 +56,6 @@ public class PathWithArray implements Path {
         int i = length - 1;
         while (i >= 0) {
             path[i] = current;
-//            current = cameFrom.get(current);
             current = cameFrom[current.getX()][current.getY()];
             i--;
         }
