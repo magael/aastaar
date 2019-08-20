32877464a5fa2f8d78933e0b84b898f4388d3a7f
magael <mikael.jaakkola@helsinki.fi>
2019-08-21 01:13:19 +0300

    sketching out performance testing

--- aastaar/src/main/java/mj/aastaar/utils/PathfindingPerformanceTester.java
+++ aastaar/src/main/java/mj/aastaar/utils/PathfindingPerformanceTester.java
@@ -10,5 +10,13 @@ import mj.aastaar.algorithms.PathfindingAlgorithm;
 public class PathfindingPerformanceTester {
 
     public void runPathfindingAlgorithm(PathfindingAlgorithm algorithm, String name, int n) {
+        for (int i = 0; i < n; i++) {
+//            System.out.println("Starting " + name);
+//            int pathLength = algorithm.search(start, goal, 4);
+//            System.out.print(name + " shortest path length: " + pathLength);
+//            System.out.println(", cost: " + algorithm.getCost(goal));
+//            shortestPaths[i] = algorithm.getPath().shortestPath(goal, start, pathLength);
+//            System.out.println("Retrieved " + name + " shortest path as array \n");
+        }
     }
 }
